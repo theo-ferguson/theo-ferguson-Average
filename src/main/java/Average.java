4ac6da1ae41867dc18a36a18ba9b5bea5f0a6e7f
theo ferguson <fergy546@yahoo.com>
2023-03-01 00:36:06 +0000

    complete

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,10 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+        }
+        return (double) sum / nums.length;
     }
 }
